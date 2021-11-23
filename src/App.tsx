e3cd679f07decb7bf669ac0da52fb31dbb28c8ea
Rodrigue ROYER <rodrigue.royer@gmail.com>
2021-11-23 23:28:09 +0400

    Ajout preliminaire du scan

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import {Button, TextView, contentView, Tab, TabFolder, Constraint} from 'tabris';
 
 export class App {
+  private bscan = new esbarcodescanner.BarcodeScannerView({
+    left: 50, top: 50, right: 50, bottom: 250, id: 'BSCAN'
+  })
 
   public start() {
     contentView.append(
       <$>
         <TabFolder paging stretch selectionIndex={0} tabBarLocation='bottom'>
           <Tab id='ACCUEIL' title='Accueil'>
-            <Button center onSelect={this.showText}>Initialisation</Button>
+            <Button bottom={50} onSelect={this.showText}>Initialisation</Button>
             <TextView id='TEMP0' centerX bottom={[Constraint.prev, 20]} font={{size: 24}}/>
           </Tab>
           <Tab id='SCAN' title='Scanner' visible={false}>
-            <Button center enabled={false}>Scanner</Button>
+            <Button bottom={50} onSelect={this.startScanner}>Scanner</Button>
             <TextView id='TEMP1' centerX bottom={[Constraint.prev, 20]} font={{size: 24}} text='Partie scan a creer'/>
           </Tab>
           <Tab id='CONTENU' title='Contenu' visible={false}>
@@ -21,11 +24,17 @@ export class App {
         </TabFolder>
       </$>
     );
+    this.bscan.appendTo(contentView);
+    this.bscan.on('detect', (e) => $(TextView).only('#TEMP1').text = 'Donnee : ' + e.data)
   }
 
   private showText = () => {
     $(TextView).only('#TEMP0').text = 'Fichier a creer !';
     $(Tab).only('#SCAN').visible = true;
   };
+
+  private startScanner = () => {
+    this.bscan.start(['qr']);
+  }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,66 @@
+import { EventObject, JSXAttributes, Properties, PropertyChangedEvent, Widget } from 'tabris';
 import {App} from './App';
+
+interface BarcodeScannerViewProperties {
+    camera?: 'front' | 'back';
+    scaleMode?: 'fit' | 'fill';
+    readonly active?: boolean;
+}
+
+interface BarcodeScannerViewEvents {
+    detect?: (event: DetectEvent) => void;
+    error?: (event: ErrorEvent) => void;
+      activeChanged?: (event: ActiveChangedEvent) => void;
+}
+
+declare global {
+    namespace esbarcodescanner {
+      interface BarcodeScannerView extends BarcodeScannerViewProperties {}
+      class BarcodeScannerView extends Widget {
+        public jsxAttributes: JSXAttributes<this> & BarcodeScannerViewProperties & {
+          onDetect?: (event: DetectEvent) => void,
+          onError?: (event: ErrorEvent) => void,
+          onActiveChanged?: (event: ActiveChangedEvent) => void
+        };
+        constructor(properties: Properties<Widget> & BarcodeScannerViewProperties);
+        start(formats: BarcodeScannerFormat[]): void;
+        stop(): void;
+        on(type: string, listener: (event: any) => void, context?: object): this;
+        on(listeners: BarcodeScannerViewEvents): this;
+        off(type: string, listener: (event: any) => void, context?: object): this;
+        off(listeners: BarcodeScannerViewEvents): this;
+        once(type: string, listener: (event: any) => void, context?: object): this;
+        once(listeners: BarcodeScannerViewEvents): this;
+      }
+    }
+}
+
+type BarcodeScannerFormat =
+    'upcA' |
+    'upcE' |
+    'code39' |
+    'code39Mod43' |
+    'code93' |
+    'code128' |
+    'ean8' |
+    'ean13' |
+    'pdf417' |
+    'qr' |
+    'aztec' |
+    'interleaved2of5' |
+    'itf' |
+    'dataMatrix' |
+    'codabar';
+
+interface ErrorEvent extends EventObject<esbarcodescanner.BarcodeScannerView> {
+    error: string;
+}
+
+interface DetectEvent extends EventObject<esbarcodescanner.BarcodeScannerView> {
+    format: BarcodeScannerFormat;
+    data: string;
+}
+
+type ActiveChangedEvent = PropertyChangedEvent<esbarcodescanner.BarcodeScannerView, boolean>;
 
 new App().start();
