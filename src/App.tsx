b87a8ed47311441e3f722a71fc0ee7be15079db1
Rodrigue ROYER <rodrigue.royer@gmail.com>
2021-11-23 15:59:22 +0400

    Squelete de base par tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import {Button, TextView, contentView, Constraint} from 'tabris';
+import {Button, TextView, contentView, Tab, TabFolder, Constraint} from 'tabris';
 
 export class App {
 
   public start() {
     contentView.append(
       <$>
-        <Button center onSelect={this.showText}>Tap here</Button>
-        <TextView centerX bottom={[Constraint.prev, 20]} font={{size: 24}}/>
+        <TabFolder paging stretch selectionIndex={0} tabBarLocation='bottom'>
+          <Tab id='ACCUEIL' title='Accueil'>
+            <Button center onSelect={this.showText}>Initialisation</Button>
+            <TextView id='TEMP0' centerX bottom={[Constraint.prev, 20]} font={{size: 24}}/>
+          </Tab>
+          <Tab id='SCAN' title='Scanner' visible={false}>
+            <Button center enabled={false}>Scanner</Button>
+            <TextView id='TEMP1' centerX bottom={[Constraint.prev, 20]} font={{size: 24}} text='Partie scan a creer'/>
+          </Tab>
+          <Tab id='CONTENU' title='Contenu' visible={false}>
+            <Button center>Lister</Button>
+            <TextView id='TEMP2' centerX bottom={[Constraint.prev, 20]} font={{size: 24}} text='Partie liste a creer'/>
+          </Tab>
+        </TabFolder>
       </$>
     );
   }
 
   private showText = () => {
-    $(TextView).only().text = 'Tabris.js rocks!';
+    $(TextView).only('#TEMP0').text = 'Fichier a creer !';
+    $(Tab).only('#SCAN').visible = true;
   };
 
 }
